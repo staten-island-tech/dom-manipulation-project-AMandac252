5cfd0e2e1380773e3ffadd4ba114416ba6494ecb
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-11-09 09:53:34 -0500

    help me
    i dont know what to doe herlp me please t hanky u

---
+++ index.js
@@ -0,0 +1,19 @@
+//console.log("connected");
+// const DOMSelectors = {
+//   button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
+//   text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
+//   box: document.getElementById("big-black-box"),
+//   points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
+//   null: document.getElementById("point"), // if you try to access a class with getElementById, it will result in "null"
+//   empty: document.querySelector(".nothing"),
+// };
+//make cards -> make a function that allows the creation of cards i.e can be about albums or anythign that allows cards
+
+const DOMSelectors = {
+  text: document.querySelector("post"),
+  button: document.querySelector("btn"),
+  userList: document.querySelectorAll("list"),
+};
+
+text.classList.add("post");
+button.addEventListener("click", function () {});
