f791b463c5ec6214224098fa664951d4573abde4
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-11-15 10:15:46 -0500

    Beeg js must check

--- index.js
+++ index.js
@@ -9,14 +9,30 @@
 // };
 //make cards -> make a function that allows the creation of cards i.e can be about albums or anythign that allows cards
 
-const DOMSelectors = {
-  text: document.querySelector("post"),
-  button: document.querySelector("btn"),
-};
+// const DOMSelectors = {
+//   text: document.querySelector("post"),
+//   button: document.querySelector("btn"),
+// };
 
-text.classList.add("post");
-button.addEventListener("click", function () {});
+// text.classList.add("post");
+// button.addEventListener("click", function () {});
 
 //get data from info from user and save it
 //make user info into an object
 //push data into html
+//target the form and not the button
+//log the event(form) and then get the values from the form
+
+const DOMSelectors = {
+  title: document.getElementsByClassName("input1"),
+  image: document.getElementsByClassName("input2"),
+  link: document.getElementsByClassName("input3"),
+};
+
+<div class="display-card" id="card">
+  <h2 class="card-header">${input1}</h2>
+  <img src="${input}" alt="song image" class="card-img" />
+  <h3 class="card-link">${input3}</h3>
+
+  <button type="button" id="remove-card"></button>
+</div>;
