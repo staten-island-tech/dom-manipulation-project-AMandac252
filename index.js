f7eac3bfbbc246562addb6e9b83e28a8c296fa01
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-11-21 21:01:24 -0500

    i wnat to die harder

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 //console.log("connected");
 
-//get data from info from user and save it
-//make user info into an object
-//push data into html
-//target the form and not the button
-//log the event(form) and then get the values from the form
-//seperate functional functions
-
-const DOMSelectors = {
+const DOMselectors = {
   title: document.getElementById("title"),
   artist: document.getElementById("artist"),
   image: document.getElementById("url"),
@@ -16,47 +9,37 @@ const DOMSelectors = {
   display: document.getElementById("display"),
   form: document.querySelector("form-inputs"),
 };
-//query the form
-//display
 
-DOMSelectors.button.addEventListener("submit", addItems);
-DOMSelectors.clearbtn.addEventListener("click", clearItems);
+DOMselectors.form.addEventListener("click", addItems);
+DOMselectors.clearBtn.addEventListener("click", clearItems);
 
 //additems
 function addItems(event) {
   event.preventDefault();
-  let inputs = document.createElement("div");
 
-  let title = DOMSelectors.title.value;
-  let img = DOMSelectors.image.value;
-  let artist = DOMSelectors.url.value;
+  const title = DOMselectors.title.value;
+  const img = DOMselectors.image.value;
+  const artist = DOMselectors.url.value;
 
-  inputs.insertAdjacentHTML(
+  DOMselectors.display.insertAdjacentHTML(
     "beforeend",
 
     `<div class="display-card">
-      <p class="display-album">${title}</p>
-     <p class="display-img"><img> scr="${img}"</p>
-      <p class ="display-artist">${artist}</p>
-      <button type="button" id="remove-card"></button>
-   
-    </div>`
+        <p class="display-album">${title}</p>
+       <p class="display-img"><img> scr="${img}"</p>
+        <p class ="display-artist">${artist}</p>
+        <button type="button" id="remove-card">x</button>
+     
+      </div>`
   );
-
-  DOMSelectors.title.value = "";
-  DOMSelectors.img.value = "";
-  DOMSelectors.link.value = "";
+  DOMselectors.title.value = "";
+  DOMselectors.img.value = "";
+  DOMselectors.link.value = "";
 }
-// read the values
-// create object from the values
-// create and push HTML
 
-// function clearItems() {
-//   const card = document.querySelectorAll("display");
-//   const remove = document.querySelectorAll("");
+const card = document.querySelectorAll("display-card");
+const remove = document.querySelectorAll("#removecard");
 
-//   remove.forEach((card) => {});
-//   function removeEventListener() {
-//     remove.addEventListener("click", function () {});
-//   }
-// });
+// remove.forEach((card) => {});
+// function removeEventListener() {
+//   remove.addEventListener("click", function () {});
