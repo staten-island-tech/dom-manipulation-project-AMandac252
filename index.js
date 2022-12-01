431e6808b0e16d9f5c7aea8a2442603235c3668d
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-01 09:19:13 -0500

    why it work

--- index.js
+++ index.js
@@ -8,27 +8,20 @@ const DOMSelectors = {
   display: document.querySelector("#display"),
 };
 
+let title = null;
+let img = null;
+let artist = null;
+
 DOMSelectors.button.addEventListener("click", function (e) {
   e.preventDefault();
-  const title = DOMSelectors.title.value;
-  const img = DOMSelectors.image.value;
-  const artist = DOMSelectors.artist.value;
 
-  function addcard() {
-    DOMSelectors.display.insertAdjacentHTML(
-      "beforeend",
+  title = DOMSelectors.title.value;
+  img = DOMSelectors.image.value;
+  artist = DOMSelectors.artist.value;
 
-      `<div class="display-card">
-                <p class="display-album">${title}</p>
-               <p><img class="display-img" src="${img}"></p>
-                <p class ="display-artist">${artist}</p>
-                <button class="remove btn">Remove Album</button>
-              </div>`
-    );
-
-    clearinput();
-  }
+  clearinput();
   addcard();
+
   document.querySelectorAll(".remove").forEach((element) =>
     element.addEventListener("click", function (e) {
       e.target.parentElement.remove();
@@ -41,4 +34,17 @@ function clearinput() {
   DOMSelectors.title.value = "";
   DOMSelectors.artist.value = "";
   DOMSelectors.image.value = "";
+}
+
+function addcard() {
+  DOMSelectors.display.insertAdjacentHTML(
+    "beforeend",
+
+    `<div class="display-card">
+              <p class="display-album">${title}</p>
+             <p><img class="display-img" src="${img}"></p>
+              <p class ="display-artist">${artist}</p>
+              <button class="remove btn">Remove Album</button>
+            </div>`
+  );
 }
